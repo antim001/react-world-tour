90e3c09a7e45657ddffeababd3b499fff6a0d008
Antim <you@example.com>
2024-01-04 00:30:38 +0600

    adding visited countries with event handling

--- src/assets/components/countries/Countries.jsx
+++ src/assets/components/countries/Countries.jsx
@@ -5,18 +5,32 @@ import Country from "../country/Country";
 
 const Countries = () => {
     const [countries,setCountries]=useState([])
+    const [visitedCountries,setVisitedCountries]=useState([])
+    
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
         .then(data=>setCountries(data))
     },[])
-
+const handleVisitedCountry=country=>{
+      
+           const newvisitedCountries=[...visitedCountries,country]
+           setVisitedCountries(newvisitedCountries)
+}
     return (
         <div>
          <h3>Countries:{countries.length}</h3> 
+         <h3 className="visited"> Visited countries:{visitedCountries.length}</h3>
+         <ul>
+         {
+            visitedCountries.map(country=><li key={country.cca3}>{country.name.common}</li>)
+         }
+         </ul>
          <div className="country-container">
          {
-            countries.map((c)=><Country key={c.cca3} country={c}></Country>
+            countries.map((c)=><Country
+            handleVisitedCountry={handleVisitedCountry}
+            key={c.cca3} country={c}></Country>
             )
          } </div> 
         </div>
--- src/assets/components/country/Country.jsx
+++ src/assets/components/country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './country.css'
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountry}) => {
     const {name,flags,population,area,cca3}=country;
     const [visited,setVisited]=useState(false)
   const handleButton=()=>{
@@ -13,6 +13,8 @@ const Country = ({country}) => {
            <h3>Population:{population}</h3>
            <h3>Area:{area}</h3>
            <h3><small>Code:{cca3}</small></h3>
+           <button onClick={()=>handleVisitedCountry(country)}>Mark Visited</button>
+           <br />
            <button onClick={handleButton}>{visited?'Ghurchi':'Ghurte jabo'}</button>
            {
              visited? 'Desh ghurchi ami':'Taka jommachi,tarpor jabo'
